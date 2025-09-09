11da9b196e0d54bdf30e240181345c156c180c3c
ismail enes hatipoglu <ismaileneshatipoglu@gmail.com>
2025-09-09 13:56:11 +0300

    Projenin Entity,Class,Migration işlemleri ayrı bölüm olarak da Paketlerin kurulması işlemleri gerçekleştirilmiştir.ç

---
+++ MyPortfolioUdemy/DAL/Entities/About.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolioUdemy.DAL.Entities
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string SubDescription { get; set; }
+        public string Details { get; set; }
+    }
+}
