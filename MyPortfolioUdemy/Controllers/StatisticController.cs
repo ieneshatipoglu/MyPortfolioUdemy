f0d37d89680032f01d53bf6c1440bbd5319f55a4
ismail enes hatipoglu <ismaileneshatipoglu@gmail.com>
2025-09-12 14:30:47 +0300

    Son olarak projemizde ileti ekleme güncelleme silme işlemlerini de tanımlamış olduk.Gelen okunmamış bildirimlerin de gözükmesini sağladık.Projenin bazı kısımları tamamlanmadı.Ancak genel itibarıyla harika geliştirici bir projeydi.

---
+++ MyPortfolioUdemy/Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using MyPortfolioUdemy.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class StatisticController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Messages.Where(x=> x.IsRead == false).Count();
+            ViewBag.v4 = context.Messages.Where(x=> x.IsRead == true).Count();
+            return View();
+        }
+    }
+}
